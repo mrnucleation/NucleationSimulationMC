f07877027e8dfd430d2bb95c472d7c5a8908655f
tloeff <tloeff1@lsu.edu>
2018-05-01 14:36:43 -0500

    Minor clean up

--- src/EnergyFunctions/LJ_Q/LJ_Electro_Functions.f90
+++ src/EnergyFunctions/LJ_Q/LJ_Electro_Functions.f90
@@ -16,7 +16,7 @@
 !*********************************************************************************************************************
       module InterEnergy_LJ_Electro
       use VarPrecision
-      real(dp), parameter :: lj_Cut = 800.0
+      real(dp), parameter :: lj_Cut = 8.5
       real(dp), parameter :: lj_Cut_sq = lj_Cut**2
 
       real(dp), parameter :: q_Cut = 800.5
